50fd4cf32af36d003e45f2353461621ebebe1436
Thadeu <thadeu.russo@caelum.com.br>
2012-04-08 17:08:12 -0300

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Thadeu Carmo on 4/8/12.
+//  Copyright (c) 2012 IME-USP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Thadeu Carmo on 4/8/12.
+//  Copyright (c) 2012 IME-USP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
