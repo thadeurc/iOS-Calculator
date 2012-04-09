9b4f989dabdaaff229aca6f054a315b5cd26d2d1
Thadeu <thadeu.russo@caelum.com.br>
2012-04-09 00:34:53 -0300

    base code and + and * implemented

---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,53 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by Thadeu Carmo on 4/8/12.
+//  Copyright (c) 2012 IME-USP. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@interface CalculatorBrain()
+@property (nonatomic, strong) NSMutableArray *operandStack;
+@end
+
+
+@implementation CalculatorBrain
+
+@synthesize operandStack = _operandStack;
+
+- (NSMutableArray *) operandStack {
+    if(!_operandStack){
+        _operandStack = [[NSMutableArray alloc] init];
+    }
+    return _operandStack;
+}
+
+- (double) popOperand{
+    NSNumber *operandObject = [self.operandStack lastObject];
+    if(operandObject){
+        [self.operandStack removeLastObject];   
+    }
+    return [operandObject doubleValue];
+}
+
+- (void) pushOperand: (double) operand {
+    [self.operandStack addObject:[NSNumber numberWithDouble:operand]];
+}
+
+- (double) performOperation: (NSString *) operation {
+    double result = 0.0;
+    if([operation isEqualToString:@"+"]){
+        result = [self popOperand] + [self popOperand];
+    }
+    // sending a message to a constant string
+    else if([@"*" isEqualToString:operation]){
+        result = [self popOperand] * [self popOperand];
+    }
+    [self pushOperand:result];
+    return result;
+}
+
+
+@end
--- Calculator/CalculatorViewController.m
+++ Calculator/CalculatorViewController.m
@@ -7,28 +7,52 @@
 //
 
 #import "CalculatorViewController.h"
+#import "CalculatorBrain.h"
 
 @interface CalculatorViewController ()
+
+@property (nonatomic) BOOL userIsInTheMiddleOfEnteringNumber;
+@property (nonatomic, strong) CalculatorBrain *brain;
 
 @end
 
 @implementation CalculatorViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+@synthesize brain   = _brain;
+@synthesize display = _display;
+@synthesize userIsInTheMiddleOfEnteringNumber = _userIsInTheMiddleOfEnteringNumber;
+
+- (CalculatorBrain *) brain {
+    if(!_brain){
+        _brain = [[CalculatorBrain alloc] init];
+    }
+    return _brain;
 }
 
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+- (IBAction)digitPressed:(UIButton *)sender {
+
+    NSString *digit = [sender currentTitle];
+    if(self.userIsInTheMiddleOfEnteringNumber){
+        self.display.text = [self.display.text stringByAppendingString:digit];
+    }
+    else{
+        self.display.text = digit;
+        self.userIsInTheMiddleOfEnteringNumber = YES;
+    }
+}
+- (IBAction)operationPressed:(UIButton *)sender {
+    if(self.userIsInTheMiddleOfEnteringNumber){
+        [self enterPressed];
+    }
+    double result = [self.brain performOperation:sender.currentTitle];
+    NSString *resultAsString = [NSString stringWithFormat:@"%g", result];
+    self.display.text = resultAsString;
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+- (IBAction)enterPressed {
+    double value = [self.display.text doubleValue];
+    [self.brain pushOperand:value];
+    self.userIsInTheMiddleOfEnteringNumber = NO;
 }
 
 @end
